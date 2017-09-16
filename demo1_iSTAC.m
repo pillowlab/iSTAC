71d9b1c7f74a1f138761173b82189c7afb897e5f
pillow <pillow@linus.fios-router.home>
2017-09-16 01:45:32 -0400

    made demo2 to show fitting of iSTAC nonlinearity and evaluating log-likelihood

--- demo1_iSTAC.m
+++ demo1_iSTAC.m
@@ -6,7 +6,7 @@
 nt = 20;        % number of temporal elements of filter
 tvec = (-nt+1:0)'; % vector of time indices (in units of stim frames)
 
-% Make some filters
+% Make some temporal filters
 filt1 = exp(-((tvec+4.5)/1.5).^2/2) -.2*exp(-((tvec+nt/2)/3).^2/2); %1st filter
 filt1 = filt1./norm(filt1);  %normalize
 
@@ -33,20 +33,22 @@ r = max(linresp,0)*50; % instantaneous spike rate (rectified linear nonlinearity
 spikes = poissrnd(r/RefreshRate); % generate spikes
 
 [sta,stc,rawmu,rawcov] = simpleSTC(Stim,spikes, nt); % Compute STA and STC
-[u,s,v] = svd(stc); % Compute eigenvectors of STC matrix
+[u,~,~] = svd(stc); % Compute eigenvectors of STC matrix
 
-ndims = 1;
-[vecs, vals, DD] = compiSTAC(sta, stc, rawmu, rawcov, ndims);
-plot(tvec, filt1, 'k--', tvec, sta./norm(sta), tvec, u(:,end), tvec, vecs);
+% Compute iSTAC estimator
+nfilts = 1;
+istacfilts = compiSTAC(sta, stc, rawmu, rawcov, nfilts);
+plot(tvec, filt1, 'k--', tvec, sta./norm(sta), tvec, u(:,end), tvec, istacfilts);
 legend('true k', 'STA', 'STC', 'iSTAC', 'location', 'northwest');
-xlabel('time before spike'); ylabel('filter coeff');
+xlabel('time before spike (bins)'); ylabel('filter coeff');
+title('filter estimates');
 
-Errs = [subspace(filt1,sta) subspace(filt1, u(:,end)) subspace(filt1, vecs)];
+% Compute error (using subspace angle between true filter and estimates)
+Errs = [subspace(filt1,sta) subspace(filt1, u(:,end)) subspace(filt1, istacfilts)];
 fprintf(1, 'Errors: STA=%.3f, STC=%.3f, iSTAC=%.3f\n', Errs(1), Errs(2), Errs(3));
 
 
-%% 2. 2nd example:  2-dimensional LNP model with a quadratic nonlinearity,
-% non-zero STA, and a correlated stimulus
+%% 2. 2nd example:  2-filter LNP model with quadratic nonlinearity, correlated stimuli
 % ===================================================
 %
 % In this example, the STA also lies in the space spanned by the two
@@ -67,39 +69,39 @@ spikes = poissrnd(r/RefreshRate); % generate spikes
 [sta,stc,rawmu,rawcov] = simpleSTC(Stim,spikes,nt);
 [u,s,v] = svd(stc);
 
-ndims = 10;  % (Only need 2, but compute 10 for demonstration purposes)
+nfilts = 10;  % (Only need 2, but compute 10 for demonstration purposes)
 eigvalthresh = 0.05; % eigenvalue cutoff threshold (for pruning dims from raw stimulus)
-[vecs, vals, DD] = compiSTAC(sta, stc, rawmu, rawcov, ndims,eigvalthresh);
+[istacfilts,vals] = compiSTAC(sta, stc, rawmu, rawcov, nfilts,eigvalthresh);
 KLcontributed = [vals(1); diff(vals)];
-ndims = length(vals);
+nfilts = length(vals);
 
-subplot(221);  plot(1:ndims, KLcontributed, 'o');
+subplot(221);  plot(1:nfilts, KLcontributed, 'o');
 title('KL contribution');
 xlabel('subspace dimensionality');
 
 subplot(221);
 plot(tvec, filt1, 'k--', tvec, u(:,1:2)*u(:,1:2)'*filt1, ...
-    tvec, vecs(:,1:2)*vecs(:,1:2)'*filt1, 'r');
+    tvec, istacfilts(:,1:2)*istacfilts(:,1:2)'*filt1, 'r');
 title('Reconstruction of 1st filter'); ylabel('filter coeff');
 legend('true k', 'STC', 'iSTAC', 'location', 'northwest');
 
 subplot(223);
 plot(tvec, filt2, 'k--', tvec, u(:,1:2)*u(:,1:2)'*filt2, ...
-    tvec, vecs(:,1:2)*vecs(:,1:2)'*filt2, 'r');
+    tvec, istacfilts(:,1:2)*istacfilts(:,1:2)'*filt2, 'r');
 title('Reconstruction of 2nd filter');
 xlabel('time before spike'); ylabel('filter coeff');
 
 subplot(222);  
-plot(1:ndims, KLcontributed, 'o');
+plot(1:nfilts, KLcontributed, 'o');
 title('KL contribution');
 xlabel('subspace dimensionality');
 
 subplot(224);
-plot(tvec, vecs(:,1:2));
+plot(tvec, istacfilts(:,1:2));
 title('iSTAC filters');
 legend('1st', '2nd', 'location', 'northwest');
 
-Errs = [subspace([filt1 filt2], u(:,1:2)) subspace([filt1 filt2], vecs(:,1:2))];
+Errs = [subspace([filt1 filt2], u(:,1:2)) subspace([filt1 filt2], istacfilts(:,1:2))];
 fprintf(1, 'Errors: STC=%.3f, iSTAC=%.3f\n', Errs(1), Errs(2));
 
 
@@ -110,7 +112,7 @@ fprintf(1, 'Errors: STC=%.3f, iSTAC=%.3f\n', Errs(1), Errs(2));
 slens = [1000 4000 16000 64000]; % number of samples for different expts
 nexpts = length(slens);  % number of experiments
 niters = 20;  % number of times to repeat each experiment
-ndims = 1; % number of filters to estimate 
+nfilts = 1; % number of filters to estimate 
 
 % Initialize matrices for storing errors
 ErrsSTA = zeros(niters, nexpts);
@@ -129,10 +131,10 @@ for i = 1:nexpts
         spikes = poissrnd(r/RefreshRate); % generate spikes
 
         [sta,stc,rawmu,rawcov] = simpleSTC(Stim,spikes, nt); % Compute STA and STC
-        istac =  compiSTAC(sta, stc, rawmu, rawcov, ndims);  % compute iSTAC estimate
+        istacFilt =  compiSTAC(sta, stc, rawmu, rawcov, nfilts);  % compute iSTAC estimate
 
         ErrsSTA(j,i) = subspace(filt1,sta); 
-        ErrsiSTAC(j,i)= subspace(filt1, istac);
+        ErrsiSTAC(j,i)= subspace(filt1, istacFilt);
     end
 end
 
@@ -143,4 +145,3 @@ set(gca, 'xscale', 'log', 'yscale', 'linear');
 legend('STA', 'iSTAC', 'location', 'northeast');
 xlabel('stim length (samples)');
 ylabel('error (radians)');
-
